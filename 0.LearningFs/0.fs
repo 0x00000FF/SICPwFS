a6385a11d6af67bd7cc17b4070cddd031d9d9761
P.Knowledge <doctor@kangnang.net>
2018-08-10 09:27:12 +0900

    Start

--- 0.LearningFs/0.fs
+++ 0.LearningFs/0.fs
@@ -1,9 +1,16 @@
-// Referred tutorialspoint.com, F# Basic Tutorial.
+(*
+    This is a preparation for SICPwFS project, I have no F# skills.
+    So I decided to learn basics briefly.
+    
+    Referred tutorialspoint.com, F# Basic Tutorial.
+    https://www.tutorialspoint.com/fsharp/index.htm
+*)
 
+// to use .NET features, use open
 open System //; F# does not put semicolon to the end of line.
 
-let somefunc =
-  3 // to return value, just write the value what you want to return.
+let somefunc x =
+  x + 3 // to return a value, just write the value what you want to return.
 
 let main() = 
   // .NET featrues can be used in F#
